de66983a04ea71ca0a6522a827bfa66e2083a16d
Phan Ngưng <72738828+77x17@users.noreply.github.com>
2024-01-21 11:56:06 +0700

    Add files via upload

--- autotickexitbox.py
+++ autotickexitbox.py
@@ -1,7 +1,6 @@
 import cv2
 import time
 import subprocess
-import xml.etree.ElementTree as ET
 from PIL import Image
   
 run     = True
@@ -11,95 +10,6 @@ N_ARROW = 2
 
 def run_command(command):
     subprocess.run(command, shell=True, text=True)
-
-
-def check_resource_id_in_xml(resource_id_to_check, app, clickable = "true"):
-    xml_file = 'window_dump.xml'
-
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    def check_resource_id_exists(element, resource_id):
-        return resource_id in element.attrib.get('resource-id', '') and element.attrib['package'] == app and element.attrib['clickable'] == clickable 
-
-    resource_id_found = any(check_resource_id_exists(element, resource_id_to_check) for element in root.iter())
-
-    if resource_id_found:
-        print(f"Resource ID '{resource_id_to_check}' found in the XML.")
-        return True
-    else:
-        print(f"Resource ID '{resource_id_to_check}' not found in the XML.")
-        return False
-
-
-def check_text_in_xml(text_to_check, app, clickable = "true"):
-    xml_file = 'window_dump.xml'
-
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    def check_resource_id_exists(element, text):
-        return text in element.attrib.get('text', '') and element.attrib['package'] == app and element.attrib['clickable'] == clickable
-
-    text_found = any(check_resource_id_exists(element, text_to_check) for element in root.iter())
-
-    if text_found:
-        print(f"Text '{text_to_check}' found in the XML.")
-        return True
-    else:
-        print(f"Text '{text_to_check}' not found in the XML.")
-        return False
-
-
-def get_bounds_for_resource_id(resource_id):
-    xml_file = 'window_dump.xml'
-
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    def get_bounds(element):
-        values = [int(value) if value.isdigit() else 0 for value in element.attrib.get('bounds', '').replace('][', ',').strip('[]').split(',')]
-        return (values[0] + values[2]) / 2, (values[1] + values[3]) / 2
-
-    for element in root.iter():
-        if 'resource-id' in element.attrib and element.attrib['resource-id'] == resource_id:
-            return get_bounds(element)
-
-    return None
-
-
-def get_bounds_for_text(text):
-    xml_file = 'window_dump.xml'
-
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    def get_bounds(element):
-        values = [int(value) if value.isdigit() else 0 for value in element.attrib.get('bounds', '').replace('][', ',').strip('[]').split(',')]
-        return (values[0] + values[2]) / 2, (values[1] + values[3]) / 2
-
-    for element in root.iter():
-        if 'text' in element.attrib and element.attrib['text'] == text:
-            return get_bounds(element)
-
-    return None
-
-
-def get_answer_plus():
-    xml_file = 'window_dump.xml'
-
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    def get_text(element):
-        values = [int(value) if value.isdigit() else 0 for value in element.attrib.get('text', '').replace(' ', '').split('+')]
-        return values[0] + values[1]
-
-    for element in root.iter():
-        if 'resource-id' in element.attrib and element.attrib['resource-id'] == "com.efast.efree:id/tvMath":
-            return get_text(element)
-
-    return None
 
 
 def take_screenshot(width = 1080, height = 1920):
@@ -150,47 +60,20 @@ def efast_efree():
         take_screenshot()
 
     print("ok")
-    if findLocation("ok.png"):
+    if findLocation("ok.png", False):
+        if findLocation("limit.png"):
+            global run
+            run = False
+
+        findLocation("ok.png")
+
         take_screenshot()
         
         return
-    
-    # if findLocation("ok.png", False):
-    #     run_command("adb shell uiautomator dump && adb pull /sdcard/window_dump.xml")
-
-    #     if check_text_in_xml("Oops, you've exceeded the limit", "com.efast.efree", "false"):
-    #         global run 
-    #         run = False
-            
-    #     findLocation("ok.png")
-        
-    #     take_screenshot()
-
-    #     return
 
     print("equation")
     while findLocation("equation.png"):
         take_screenshot()
-
-    # if findLocation("equation.png", False):
-    #     run_command("adb shell input keyevent 3")
-    #     run_command("adb shell monkey -p com.efast.efree -c android.intent.category.LAUNCHER 1")
-        
-    #     run_command("adb shell uiautomator dump && adb pull /sdcard/window_dump.xml")
-        
-    #     if check_resource_id_in_xml("com.efast.efree:id/ivCheckUser", "com.efast.efree"):
-    #         x, y = get_bounds_for_resource_id("com.efast.efree:id/etResultUser")
-    #         run_command(f"adb shell input tap {x} {y}")
-
-    #         run_command(f"adb shell input text {get_answer_plus()}")
-            
-    #         run_command("adb shell input keyevent 4")
-    #         run_command("adb shell input keyevent 4")
-
-    #         x, y = get_bounds_for_resource_id("com.efast.efree:id/ivCheckUser")
-    #         run_command(f"adb shell input tap {x} {y}")
-        
-    #     take_screenshot()
 
     print("------------------------------------\nads")
     
